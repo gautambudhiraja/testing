44d44dd5d107fe4d6529d35a4d2a906e1710e771
gautambudhiraja <37044923+gautambudhiraja@users.noreply.github.com>
2026-02-24 18:35:04 +0530

    Rename class from helloworld to HelloWorld

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,4 @@
-public class helloworld{
+public class HelloWorld{
     public static void main(String[] args) {
         // Print message to the console
         System.out.println("Hello, World!");
